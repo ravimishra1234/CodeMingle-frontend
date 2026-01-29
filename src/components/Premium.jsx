c85241e676cef25f364abde0ac42013c843039b1
I-Satyam-Shandhilya <106227195+I-Satyam-Shandhilya@users.noreply.github.com>
2026-01-29 23:48:27 +0530

    is Premium Added

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,7 +1,19 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+    if (res.data.isPremium) {
+      setIsUserPremium(true)
+    }
+  };
+
   const handleBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -11,29 +23,32 @@ const Premium = () => {
 
     // now it should open the razorpay dialog box
 
-    const {amount, keyId, currency, notes, orderId} = order.data
+    const { amount, keyId, currency, notes, orderId } = order.data;
 
     const options = {
-        key: keyId, 
-        amount, 
-        currency,
-        name: 'Code Mingle',
-        description: 'Connect to other developers',
-        order_id: orderId,  
-        prefill: {
-          name: notes.firstName + " " + notes.lastName,
-          email: notes.emailId,
-          contact: '9999999999'
-        },
-        theme: {
-          color: '#F37254'
-        },
-      };
+      key: keyId,
+      amount,
+      currency,
+      name: "Code Mingle",
+      description: "Connect to other developers",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#F37254",
+      },
+      handler: verifyPremiumUser,
+    };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
-  return (
+  return isUserPremium ? (
+    "You're already a premium user"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-neutral rounded-box grid h-80 grow place-items-center">
