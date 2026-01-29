1ccce2787f5ec86701b1bcdb6b3f3b6f5bcda69e
I-Satyam-Shandhilya <106227195+I-Satyam-Shandhilya@users.noreply.github.com>
2026-01-30 00:38:46 +0530

    update premium verification on page load

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Premium = () => {
   const [isUserPremium, setIsUserPremium] = useState(false);
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
 
   const verifyPremiumUser = async () => {
     const res = await axios.get(BASE_URL + "/premium/verify", {
       withCredentials: true,
     });
     if (res.data.isPremium) {
-      setIsUserPremium(true)
+      setIsUserPremium(true);
     }
   };
 
